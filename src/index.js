e120b2b7de3e21fabd723b7eebd9be8b8eb9ac53
Bharath Reddy Nagireddy <Bharathreddynagireddy@gmail.com>
2024-10-14 17:05:24 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+import './style.css';
+import $ from 'jquery';
+
+console.log('Hello, World!');
+
+$(function() {
+    
+console.log('jQuery is working!');
+
+    });
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+export default{
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js', // Use [name] to ensure unique filenames for each chunk
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
